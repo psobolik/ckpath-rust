47b7e44519e197c7281106837eef3e0b45546059
Paul Sobolik <psobolik@zoho.com>
2023-10-16 13:41:44 -0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,43 @@
 use colored::Colorize;
 use std::env;
-use std::fs;
 use std::path::PathBuf;
 
 static PATH: &str = "PATH";
 
 fn main() {
     match env::var_os(PATH) {
-        Some(ev_path) => {
-            // eprintln!("{:?}", ev_path);
-            // Collect non-blank items into a vector
-            let paths: Vec<PathBuf> = env::split_paths(&ev_path)
-                .filter(|path_buf| !path_buf.display().to_string().is_empty())
-                .collect();
-            for path in &paths {
-                let path_string = path
-                    .to_str()
-                    .expect("Can't convert path element to string. (This should never happen.)");
-                match fs::metadata(path) {
-                    Ok(metadata) => {
-                        if metadata.is_dir() {
-                            if muliple_occurrences(&paths, path) {
-                                println!(
-                                    "{} {}",
-                                    path_string.bright_yellow(),
-                                    "[Appears more than once]".bright_red()
-                                );
-                            } else {
-                                println!("{}", path_string);
-                            }
-                        } else {
-                            println!(
-                                "{} {}",
-                                path_string.bright_yellow(),
-                                "[Is not a directory]".bright_red()
-                            );
-                        }
-                    }
-                    Err(e) => {
-                        println!(
-                            "{} {}",
-                            path_string.bright_yellow(),
-                            format!("[{}]", e).bright_red()
-                        )
-                    }
-                }
-            }
-        }
+        Some(ev_path) => write_path(ev_path.as_os_str(), &mut std::io::stdout()),
         None => panic!("{} is not set", PATH),
     }
+}
+
+fn write_path(path: &std::ffi::OsStr, mut writer: impl std::io::Write) {
+    // Collect non-blank items into a vector (Why are there blank items?)
+    let paths: Vec<PathBuf> = env::split_paths(&path)
+        .filter(|path_buf| path_buf.capacity() > 0)
+        .collect();
+    for path in &paths {
+        let path_string = path.display().to_string();
+        if !path.exists() {
+            write_error(&path_string, "Does not exist", &mut writer);
+        } else if !path.is_dir() {
+            write_error(&path_string, "Is not a directory", &mut writer);
+        } else if muliple_occurrences(&paths, path) {
+            write_error(&path_string, "Appears more than once", &mut writer)
+        } else {
+            writeln!(writer, "{}", path_string).unwrap();
+        }
+    }
+}
+
+fn write_error(path: &String, error: &str, writer: &mut impl std::io::Write) {
+    writeln!(
+        writer,
+        "{} {}",
+        path.bright_yellow(),
+        format!("[{}]", error).bright_red()
+    )
+    .unwrap();
 }
 
 fn muliple_occurrences(paths: &[PathBuf], target_path: &PathBuf) -> bool {
