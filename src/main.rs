d6b6c6126b0db52e4bf4c925a88367a578c0a599
Paul Sobolik <psobolik@zoho.com>
2024-06-16 11:02:09 -0400

    Tweaks and fix spelling errors

--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,35 @@ static PSMODULEPATH: &str = "PSModulePath";
 #[command(author, version, about, long_about = None)]
 struct Arguments {
     #[arg(short, long, default_value_t = false, help = "Show PSModulePath instead of PATH")]
-    ps_moule_path: bool,
+    ps_module_path: bool,
 
-    #[arg(short = 's', long = "summary", help = "Show summary of problem items")]
-    problem_summary: bool,
+    #[arg(short = 's', long = "summary", help = "Only show summary of problems")]
+    problem_summary_only: bool,
 }
 
 fn main() {
     let args = Arguments::parse();
-    let ev = if args.ps_moule_path {
-        env::var_os(PSMODULEPATH)
+    let key = if args.ps_module_path {
+        PSMODULEPATH
     } else {
-        env::var_os(PATH)     
+        PATH
     };
-
+    let ev = env::var_os(key);
     match ev {
         Some(path) => {
-            write_path(path.as_os_str(), std::io::stdout(), args.problem_summary);
+            write_path(path.as_os_str(), std::io::stdout(), args.problem_summary_only);
         }
-        None => panic!("{} is not set", PATH),
+        None => panic!("Environment variable \"{}\" is not set", key),
     }
 }
 
-fn write_path(path: &std::ffi::OsStr, mut writer: impl std::io::Write, summary: bool) {
+fn write_path(path: &std::ffi::OsStr, mut writer: impl std::io::Write, summary_only: bool) {
     // Collect non-blank items into a vector (Why are there blank items?)
     let paths: Vec<PathBuf> = env::split_paths(&path)
         .filter(|path_buf| path_buf.capacity() > 0)
         .collect();
 
-    let mut non_existant: Vec<PathBuf> = vec![];
+    let mut non_existent: Vec<PathBuf> = vec![];
     let mut non_folder: Vec<PathBuf> = vec![];
     let mut duplicate: Vec<PathBuf> = vec![];
 
@@ -46,29 +46,45 @@ fn write_path(path: &std::ffi::OsStr, mut writer: impl std::io::Write, summary: 
         let path_string = path.display().to_string();
        
         if !path.exists() {
-            write_error(&path_string, "Does not exist", &mut writer);
-            non_existant.push(path.to_path_buf());
+            if !summary_only {
+                write_error_item(&path_string, "Does not exist", &mut writer);
+            }
+            non_existent.push(path.to_path_buf());
         } else if !path.is_dir() {
-            write_error(&path_string, "Is not a directory", &mut writer);
+            if !summary_only {
+                write_error_item(&path_string, "Is not a directory", &mut writer);
+            }
             non_folder.push(path.to_path_buf());
         } else if muliple_occurrences(&paths, path) {
-            write_error(&path_string, "Appears more than once", &mut writer);
+            if !summary_only {
+                write_error_item(&path_string, "Appears more than once", &mut writer);
+            }
             if !duplicate.contains(path) {
                 duplicate.push(path.to_path_buf());
             };
-        } else {
+        } else if !summary_only {
             writeln!(writer, "{}", path_string).unwrap();
         }
     }
-    if summary {
-        writeln!(writer, "\nProblem summary:").unwrap();
-        write_summary("Items that don't exist", non_existant, &mut writer);
-        write_summary("Items that are not folders", non_folder, &mut writer);
-        write_summary("Items that appear more than once", duplicate, &mut writer);
+    if summary_only {
+        if non_existent.is_empty() && non_folder.is_empty() && duplicate.is_empty() {
+            writeln!(writer, "\nNo problems found").unwrap();
+        } else {
+            writeln!(writer, "\nProblem summary:").unwrap();
+            if !non_existent.is_empty() {
+                write_summary_category("Items don't exist", non_existent, &mut writer);
+            }
+            if !non_folder.is_empty() {
+                write_summary_category("Items are not folders", non_folder, &mut writer);
+            }
+            if !duplicate.is_empty() {
+                write_summary_category("Items appear more than once", duplicate, &mut writer);
+            }
+        }
     }
 }
 
-fn write_summary(description: &str, items: Vec<PathBuf>, writer: &mut impl std::io::Write) {
+fn write_summary_category(description: &str, items: Vec<PathBuf>, writer: &mut impl std::io::Write) {
     writeln!(writer, "\n{description}:").unwrap();
     if items.capacity() > 0 {
         for item in items {
@@ -77,7 +93,7 @@ fn write_summary(description: &str, items: Vec<PathBuf>, writer: &mut impl std::
     }
 }
 
-fn write_error(path: &String, error: &str, writer: &mut impl std::io::Write) {
+fn write_error_item(path: &String, error: &str, writer: &mut impl std::io::Write) {
     writeln!(
         writer,
         "{} {}",
